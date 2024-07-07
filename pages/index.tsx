1ad5c87f2eba7a34d07ecf2a89e61108f48ed75d
oregon75 <oregon75oregon@gmail.com>
2024-07-07 12:48:11 -0700

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Paddle - Personal Development and Lifestyle Management</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Welcome to Paddle</h1>
+        <p>The Ultimate Personal Development and Lifestyle Management Solution</p>
+      </main>
+    </div>
+  )
+}
